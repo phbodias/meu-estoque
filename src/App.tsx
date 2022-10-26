b2dceeaf54429c3d3491965a6f7dbfbb52a42158
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-26 01:52:47 -0300

    :lipstick: GlobalStyle adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import GlobalStyle from "./styles/GlobalStyle";
+
 const App = () => {
-  return <div>aaaaa</div>;
+  return (
+    <div>
+      <GlobalStyle />
+      aaaaa
+    </div>
+  );
 };
 
 export default App;
