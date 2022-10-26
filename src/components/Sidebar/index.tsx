9044a18b305ed33446b1868b40fcffa73e805a10
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-26 19:08:56 -0300

    :construction: Layout inicial da sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,51 @@
+import IonIcon from "@reacticons/ionicons";
 import { Container } from "./style";
 
 const Sidebar = () => {
   return (
     <Container>
-      <p className="header">Meu estoque</p>
-      <hr />
+      <p className="top">Controle</p>
+      <div className="options">
+        <ul>
+          <p className="category">Gerenciamento</p>
+          <li>
+            <IonIcon name="list-circle-outline" className="ion-icon"></IonIcon>
+            <p>Produtos</p>
+          </li>
+          <li>
+            <IonIcon name="locate-outline" className="ion-icon"></IonIcon>
+            <p>Categorias</p>
+          </li>
+          <li>
+            <IonIcon name="layers-outline" className="ion-icon"></IonIcon>
+            <p>Fornecedores</p>
+          </li>
+        </ul>
+        <ul>
+          <p className="category">Movimentação</p>
+          <li>
+            <IonIcon
+              name="arrow-forward-outline"
+              className="ion-icon"
+            ></IonIcon>
+            <p>Entradas</p>
+          </li>
+          <li>
+            <IonIcon name="arrow-back-outline" className="ion-icon"></IonIcon>
+            <p>Saídas</p>
+          </li>
+        </ul>
+        <ul>
+          <p className="category">Administração</p>
+          <li>
+            <IonIcon
+              name="people-circle-outline"
+              className="ion-icon"
+            ></IonIcon>
+            <p>Usuários</p>
+          </li>
+        </ul>
+      </div>
     </Container>
   );
 };
--- src/components/Sidebar/style.ts
+++ src/components/Sidebar/style.ts
@@ -1,20 +1,65 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  width: 20vw;
+  width: 255px;
   height: 100vh;
-  background-color: #262526;
+  background-color: #212120;
   color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 10px;
+  box-sizing: border-box;
+  cursor: zoom-in;
 
-  .header {
+  .top {
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 100%;
+    width: 90%;
     height: 10%;
     min-height: 50px;
-    font-size: 20px;
-    cursor: zoom-in;
+    font-size: 27px;
+    font-weight: 500;
+    border-bottom: solid 1px #ffffff;
+    margin-bottom: 20px;
+  }
+
+  .options {
+    width: 90%;
+
+    ul {
+      margin-bottom: 20px;
+
+      .category {
+        font-size: 17px;
+        margin-bottom: 10px;
+      }
+
+      li {
+        width: fit-content;
+        height: 30px;
+        display: flex;
+        align-items: center;
+        margin: 10px 5px;
+        font-size: 15px;
+        padding: 3px 0;
+        color: #c5c0c6;
+        cursor: pointer;
+
+        p {
+          margin-left: 7px;
+        }
+
+        .ion-icon {
+          font-size: 25px;
+        }
+
+        :hover {
+          color: #ffffff;
+        }
+      }
+    }
   }
 `;
 
