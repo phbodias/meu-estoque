e1bfb0abcbefebdea5a8bf575ccf75c891957e1a
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-27 23:56:39 -0300

    :construction: Criada pagina de fornecedores

---
+++ src/pages/Providers/index.tsx
@@ -0,0 +1,15 @@
+import PageTitle from "../../components/PageTitle";
+
+const Providers = () => {
+  return (
+    <div>
+      <PageTitle
+        title="Fornecedores"
+        subTitle="Fornecedores cadastrados"
+        iconName="layers-outline"
+      ></PageTitle>
+    </div>
+  );
+};
+
+export default Providers;
